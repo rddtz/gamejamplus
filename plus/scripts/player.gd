32193a1cfefbe5cf4f2dabc9a0d87dc6a3eff8ef
enzolisp <enzolisp@gmail.com>
2024-10-19 14:39:41 -0300

    melhoras na movimentacao

--- plus/scripts/player.gd
+++ plus/scripts/player.gd
@@ -29,16 +29,16 @@ func _physics_process(delta: float) -> void:
 	if time_mov > 0:
 		time_mov -= delta;
 	
-	if Input.is_action_just_pressed("LEFT") or (Input.is_action_pressed("LEFT") and !time_mov):
+	if (Input.is_action_pressed("LEFT") and !time_mov):
 		rayan.target_position = Vector2(-16,0)
 		move()
-	elif Input.is_action_just_pressed("RIGHT") or (Input.is_action_pressed("RIGHT") and !time_mov):
+	elif Input.is_action_pressed("RIGHT") and !time_mov:
 		rayan.target_position = Vector2(16,0)
 		move()
-	elif Input.is_action_just_pressed("UP") or (Input.is_action_pressed("UP") and !time_mov):
+	elif (Input.is_action_pressed("UP") and !time_mov):
 		rayan.target_position = Vector2(0,-16)
 		move()
-	elif Input.is_action_just_pressed("DOWN") or (Input.is_action_pressed("DOWN") and !time_mov):
+	elif (Input.is_action_pressed("DOWN") and !time_mov):
 		rayan.target_position = Vector2(0,16)
 		move()
 	
