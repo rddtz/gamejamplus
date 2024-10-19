71d80c1e217e8eb369e52f813180170494dba024
Rayan Raddatz <rayandematos@gmail.com>
2024-10-19 08:33:59 -0300

    movimentação boa ja

--- plus/scripts/player.gd
+++ plus/scripts/player.gd
@@ -7,7 +7,11 @@ const JUMP_VELOCITY = -400.0
 const speed = 16
 @onready var rayan: RayCast2D = $RayCast2D
 
+const TIME = 10
+var time_mov := TIME
+
 func move():
+	time_mov = TIME
 	rayan.force_raycast_update()
 	if !rayan.is_colliding():	
 		position += rayan.target_position
@@ -15,16 +19,19 @@ func move():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
-	if Input.is_action_just_pressed("LEFT"):
+	if time_mov > 0:
+		time_mov -= delta;
+	
+	if Input.is_action_just_pressed("LEFT") or (Input.is_action_pressed("LEFT") and !time_mov):
 		rayan.target_position = Vector2(-16,0)
 		move()
-	elif Input.is_action_just_pressed("RIGHT"):
+	elif Input.is_action_just_pressed("RIGHT") or (Input.is_action_pressed("RIGHT") and !time_mov):
 		rayan.target_position = Vector2(16,0)
 		move()
-	elif Input.is_action_just_pressed("UP"):
+	elif Input.is_action_just_pressed("UP") or (Input.is_action_pressed("UP") and !time_mov):
 		rayan.target_position = Vector2(0,-16)
 		move()
-	elif Input.is_action_just_pressed("DOWN"):
+	elif Input.is_action_just_pressed("DOWN") or (Input.is_action_pressed("DOWN") and !time_mov):
 		rayan.target_position = Vector2(0,16)
 		move()
 		
