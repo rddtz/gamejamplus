4aa834db629244202aa5ade4763c9b2936d0cb52
Rayan Raddatz <rayandematos@gmail.com>
2024-10-19 07:51:58 -0300

    andando 1 por 1

--- plus/scripts/player.gd
+++ plus/scripts/player.gd
@@ -5,24 +5,17 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const speed = 16
 
-var velh := 0
-var velv := 0
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 
-	velh = Input.get_axis("LEFT", "RIGHT")
-	if velh:
-		velocity.x = velh * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-	#
-	
-	velv = Input.get_axis("UP", "DOWN")
-	if velv:
-		velocity.y = velv * speed
-	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
+	if Input.is_action_just_pressed("LEFT"):
+		position.x -= speed
+	elif Input.is_action_just_pressed("RIGHT"):
+		position.x += speed
+	elif Input.is_action_just_pressed("UP"):
+		position.y -= speed
+	elif Input.is_action_just_pressed("DOWN"):
+		position.y += speed
 
-	move_and_slide()
+
 		
