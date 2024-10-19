075b84b899aeeec1365f804c7c3492a8c99ca644
Rayan Raddatz <rayandematos@gmail.com>
2024-10-19 09:47:49 -0300

    on dano reset

--- plus/scripts/player.gd
+++ plus/scripts/player.gd
@@ -5,9 +5,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const speed = 16
+
+const TIME = 10
+var time_mov := TIME
 @onready var rayan: RayCast2D = $RayCast2D
 
+
+#Player status variables
+var vida := 1
+
 func move():
+	time_mov = TIME
 	rayan.force_raycast_update()
 	if !rayan.is_colliding():	
 		position += rayan.target_position
@@ -15,16 +23,24 @@ func move():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
-	if Input.is_action_just_pressed("LEFT"):
+	if time_mov > 0:
+		time_mov -= delta;
+	
+	if Input.is_action_just_pressed("LEFT") or (Input.is_action_pressed("LEFT") and !time_mov):
 		rayan.target_position = Vector2(-16,0)
 		move()
-	elif Input.is_action_just_pressed("RIGHT"):
+	elif Input.is_action_just_pressed("RIGHT") or (Input.is_action_pressed("RIGHT") and !time_mov):
 		rayan.target_position = Vector2(16,0)
 		move()
-	elif Input.is_action_just_pressed("UP"):
+	elif Input.is_action_just_pressed("UP") or (Input.is_action_pressed("UP") and !time_mov):
 		rayan.target_position = Vector2(0,-16)
 		move()
-	elif Input.is_action_just_pressed("DOWN"):
+	elif Input.is_action_just_pressed("DOWN") or (Input.is_action_pressed("DOWN") and !time_mov):
 		rayan.target_position = Vector2(0,16)
 		move()
 		
+		
+
+func _on_dano_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
