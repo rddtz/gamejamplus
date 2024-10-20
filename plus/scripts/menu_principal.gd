23d429441cf01189674f4a5fb5259ed4a7526099
enzolisp <enzolisp@gmail.com>
2024-10-20 11:13:53 -0300

    Musica menu bonitinho

--- plus/scripts/menu_principal.gd
+++ plus/scripts/menu_principal.gd
@@ -1,10 +1,12 @@
 extends Control
 
-
-
+var menuTheme = AudioServer.get_bus_index("menu theme")
+var mainTheme = AudioServer.get_bus_index("main theme")
 
 func _on_jogar_pressed() -> void:
 	#get_tree().change_scene_to_file("res://scenes/main.tscn")
+	AudioServer.set_bus_mute(menuTheme, true)
+	AudioServer.set_bus_volume_db(mainTheme, 0)
 	Global.call_transition("res://scenes/main.tscn")
 
 
