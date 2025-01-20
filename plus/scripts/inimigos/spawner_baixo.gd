2aff926821c0a1defb4d7353644b70df171612b5
iurikali <iurisiecz@gmail.com>
2025-01-20 19:58:50 -0300

    Organizei um pouquinho

---
+++ plus/scripts/inimigos/spawner_baixo.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var flecha_cena = preload("res://scenes/inimigos/flecha.tscn")
+@onready var timer: Timer = $Timer
+
+@export var active: bool = true
+@export var quantite: int = 1
+@export var speed: int = 300
+
+func _on_timer_timeout() -> void:
+	if active:
+		for i in range(quantite):
+					
+			var y_meio = 67 + 126
+			var flecha = flecha_cena.instantiate()
+			flecha.position = Vector2(92+16*randi_range(0,14), y_meio + 500)
+			flecha.speed = -speed
+			flecha.direction = 90
+			flecha.tipo = 2
+			get_tree().current_scene.add_child(flecha, true)
+			timer.set_wait_time(randf_range(1,3))
---
+++ plus/scripts/ui/menu_principal.gd
@@ -0,0 +1,45 @@
+extends Control
+
+var menuTheme = AudioServer.get_bus_index("menu theme")
+var mainTheme = AudioServer.get_bus_index("main theme")
+
+var clicou = false
+
+func _process(delta: float) -> void:
+	
+	var nome = $CanvasLayer/LineEdit.text
+	
+	if Input.is_action_just_pressed("enviar") && !clicou && len(nome) == 3:
+		#$Jogar.grab_focus()
+		Global.nome = nome
+		clicou = true
+		AudioServer.set_bus_mute(menuTheme, true)
+		AudioServer.set_bus_volume_db(mainTheme, 0)
+		Global.call_transition("res://scenes/ui/space.tscn")
+	if len(nome) < 3 and Global.primeiro:
+		$CanvasLayer/Jogar.disabled = true
+		pass
+	else:
+		$CanvasLayer/Jogar.disabled = false
+		
+	if !Global.primeiro:
+		$CanvasLayer/LineEdit.text = Global.nome
+
+func _on_jogar_pressed() -> void:
+	#get_tree().change_scene_to_file("res://scenes/main.tscn")
+	if !clicou: 
+		clicou = true
+		AudioServer.set_bus_mute(menuTheme, true)
+		AudioServer.set_bus_volume_db(mainTheme, 0)
+		Global.call_transition("res://scenes/ui/space.tscn")
+		
+func _on_leaderboard_pressed() -> void:
+	Global.call_transition("res://scenes/ui/final.tscn")
+	
+	
+func _on_sair_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_label_tree_exiting() -> void:
+	Global.primeiro = false
